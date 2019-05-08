45d0ffb8802f2f94872dcc0c55f9d9050f997478
MADAO <madao27@163.com>
2019-05-08 16:32:44 +0800

    before add playingMode

--- Music-Player/js/index.js
+++ Music-Player/js/index.js
@@ -43,17 +43,7 @@ $(function () {
   initEvents();
   function initEvents() {
     var $menu = $(".menu-box");
-    // 1.监听歌曲列表鼠标移入移出事件
-    $menu.delegate(".menu-item", "mouseenter", function () {
-      $(this).find(".song-operate, .operate-delete").css({display: "block"});
-      $(this).find(".song-duration span").css({display: "none"});
-    });
-    $menu.delegate(".menu-item", "mouseleave", function () {
-      $(this).find(".song-operate, .operate-delete").css({display: "none"});
-      $(this).find(".song-duration span").css({display: "block"});
-    });
-
-    // 2.监听歌曲列表勾选事件
+    // 2.监听列表操作的勾选事件
     $menu.delegate(".song-check i", "click",function () {
       $(this).parents("li").toggleClass("menu-checked ");
       if($(this).parents(".menu-header").length){
@@ -66,7 +56,7 @@ $(function () {
       }else{
         // 2.2全部勾选则首行勾选
         var $menuListChecked = $(".menu-header").siblings(".menu-checked ");
-        if($menuListChecked.length + 1 === $(".menu-item").length){
+        if($menuListChecked.length === $(".menu-item").length){
           $(".menu-header").addClass("menu-checked ")
         }else {
           $(".menu-header").removeClass("menu-checked ")
@@ -74,40 +64,48 @@ $(function () {
       }
     });
 
-    // 3.监听子菜单播放/暂停按钮的点击
+    // 12.监听列表操作的删除按钮点击事件
+    $(".menu-operate-delete").click(function () {
+      deleteMusic($(".menu-item.menu-checked"));
+    });
+
+    // 3.监听列表操作的清空按钮点击事件
+    $(".menu-operate-clean").click(function () {
+      deleteMusic($(".menu-item"));
+    });
+
+    // 1.监听歌曲列表的鼠标移入移出事件
+    $menu.delegate(".menu-item", "mouseenter", function () {
+      $(this).find(".song-operate, .operate-delete").css({display: "block"});
+      $(this).find(".song-duration span").css({display: "none"});
+    });
+    $menu.delegate(".menu-item", "mouseleave", function () {
+      $(this).find(".song-operate, .operate-delete").css({display: "none"});
+      $(this).find(".song-duration span").css({display: "block"});
+    });
+
+    // 3.监听子菜单的播放/暂停按钮点击事件
     $menu.delegate(".operate-play", "click",function () {
       var itemIndex = $(this).parents(".menu-item").index();
-      changeMusic(itemIndex - 1);// lyricIndex()是从1开始
+      changeMusic(itemIndex - 1);// index()是从1开始
     });
 
-    // 4.监听子菜单删除按钮的点击
+    // 4.监听子菜单的删除按钮点击事件
     $menu.delegate(".operate-delete", "click",function () {
-      var musicIndex = $(this).parents(".menu-item").index() - 1;
-      // 4.1样式上移除
-      $(this).parents(".menu-item").remove();
-       //    更新索引
-      $.each($(".menu-item"), function (i, v) {
-        $(v).find(".song-index span").text(i + 1);
-      });
-      // 4.2逻辑上移除
-      player.removeMusic(musicIndex, function (playingIndex) {
-        if(musicIndex === playingIndex){
-          changeMusic(musicIndex);
-        }
-      });
+      deleteMusic($(this).parents(".menu-item"));
     });
 
-    // 5.监听底部菜单播放/暂停按钮的点击
+    // 5.监听底部菜单的播放/暂停按钮点击事件
     $(".playing-pause").click(function () {
       changeMusic(player.playingIndex);
     });
 
-    // 6.监听底部菜单上一首按钮的点击
+    // 6.监听底部菜单的上一首按钮点击事件
     $(".playing-pre").click(function () {
       changeMusic(player.playingIndex - 1);
     });
 
-    // 7.监听底部菜单上一首按钮的点击
+    // 7.监听底部菜单的上一首按钮点击事件
     $(".playing-next").click(function () {
       changeMusic(player.playingIndex + 1);
     });
@@ -125,59 +123,69 @@ $(function () {
       var lyricIndex = lyric.locateLyric(currentTime);
       if(lyricIndex < 0) return;
       var $curLyric = $lyricContainer.children().eq(lyricIndex);
-      var curLyric = $lyricContainer.children().get(lyricIndex);
       $lyricContainer.children().removeClass("playing-lyric-this");
       $curLyric.addClass("playing-lyric-this");
       var boxHeight = $(".playing-lyric-box").height();
-      var top = - curLyric.offsetTop + boxHeight / 2 - curLyric.clientHeight / 2;
+      var top = - $curLyric[0].offsetTop + boxHeight / 2 - $curLyric[0].clientHeight / 2;
       if(top > 0) return;
       $lyricContainer.css({top: top});
-      // 哔了狗了，为啥animate方法一直类型报错
+      // 哔了狗，为啥animate方法一直类型报错, 暂时没查出来原因
       // $lyricContainer.stop().animate({top: top}, 100);
 
-      // 8.3播完自动切换下一首
+      // 8.3播完后根据播放模式切换下一首
       if(currentTime >= duration){
-        changeMusic(player.playingIndex + 1);
+        var reg = /playing-mode-([a-z]*)/;
+        var $playMode = $(".playing-operate-mode");
+        var curMode = reg.exec($playMode.attr("class"));
+        var nextIndex = {
+          circulate: player.playingIndex + 1,
+          random: Math.floor(Math.random() * player.musicList.length),
+          one: player.playingIndex,
+        };
+        curMode[1] === "one" && player.setPlayingIndex(-1);
+        changeMusic(nextIndex[curMode[1]]);
       }
     });
 
-    // 9.初始化歌曲进度条事件
+    // 9.监听底部菜单的歌曲进度条的点击、拖动事件
     timeProgress.start(function () {
       var length = $(".playing-progress-bar .progress-track").width();
       player.musicSeekTo(length / $timeBar.width());
     });
 
-    // 10.监听声音图标的点击
+    // 8.监听底部菜单的播放模式按钮点击事件
+    $(".playing-operate-mode").click(function () {
+      var modes = ["playing-mode-circulate", "playing-mode-one", "playing-mode-random"];
+      var reg = /playing-mode-[a-z]*/;
+      var curMode = reg.exec($(this).attr("class"));
+      var next = (modes.indexOf(curMode[0]) + 1) % modes.length;
+      $(this).toggleClass(curMode[0] + " " + modes[next]);
+    });
+    // 10.监听底部菜单的纯净模式按钮点击事件
+
+    // 10.监听底部菜单的静音按钮点击事件
     $(".playing-operate-volume").click(function () {
       player.volumeSeekTo();
     });
 
-    // 11.初始化声音进度条事件
+    // 11.监听底部菜单的声音进度条点击、拖动事件
     volumeProgress.start(function () {
       var length = $(".playing-volume-bar .progress-track").width();
       player.volumeSeekTo( length / $volumeBar.width());
     }, true);
-
-    // 12.监听列表操作的删除按钮事件
-    $(".menu-operate-delete").click(function () {
-      deleteMusic($(".menu-item.menu-checked"));
-    });
   }
 
   // 3 定义切换歌曲的方法
   function changeMusic(index) {
+    // if(!player.musicList.length) return;
     //切换当前播放曲目
     player.playMusic(index);
 
     //样式上修改当前播放曲目
     // 1.列表中设置当前播放曲目高亮
+    $(".menu-item").removeClass("menu-playing");
     var $curMenuItem = $(".menu-item").eq(player.playingIndex);
-    if(player.playing){
-      $curMenuItem.addClass("menu-playing");
-    }else {
-      $curMenuItem.removeClass("menu-playing");
-    }
-    $curMenuItem.siblings().removeClass("menu-playing");
+    player.playing && $curMenuItem.addClass("menu-playing");
 
     // 2.底部播放按钮改变
     if(player.playing){
@@ -187,22 +195,17 @@ $(function () {
     }
 
     // 3.底部显示当前播放曲目信息等
-    var name = player.musicList[player.playingIndex].name;
-    var singer = player.musicList[player.playingIndex].singer;
-    var time = player.musicList[player.playingIndex].time;
-    var cover = player.musicList[player.playingIndex].cover;
-    var album = player.musicList[player.playingIndex].album;
-    var lyricUrl = player.musicList[player.playingIndex].link_lrc;
-    $(".playing-name").text(name);
-    $(".playing-singer").text(singer);
-    $(".playing-duration").text(time);
+    var music = player.musicList[player.playingIndex];
+    $(".playing-name").text(music.name);
+    $(".playing-singer").text(music.singer);
+    $(".playing-duration").text(music.time);
 
     // 4.侧边显示当前播放曲目歌词信息等
-    $(".playing-info-poster").css({backgroundImage: "url("+ cover +")"});
-    $(".playing-info-name a").text(name);
-    $(".playing-info-singer a").text(singer);
-    $(".playing-info-album a").text(album);
-    lyric.loadLyric(lyricUrl, function (lyricArr) {
+    $(".playing-info-poster").css({backgroundImage: "url("+ music.cover +")"});
+    $(".playing-info-name a").text(music.name);
+    $(".playing-info-singer a").text(music.singer);
+    $(".playing-info-album a").text(music.album);
+    lyric.loadLyric(music.link_lrc, function (lyricArr) {
       //清空上一首歌的歌词
       $lyricContainer.html("");
       //载入当前播放歌曲的歌词
@@ -213,33 +216,28 @@ $(function () {
     });
 
     // 5.切换页面背景
-    $(".player-mask").css({backgroundImage: "url("+ cover +")"});
+    $(".player-mask").css({backgroundImage: "url("+ music.cover +")"});
   }
   
   // 4 定义删除歌曲的方法
-  function deleteMusic($items) {
-    var indexs = [];
-    $.each($items, function (i, v) {
-      indexs.push($(v).index() - 1);
+  function deleteMusic($item) {
+    // 4.1获取移除元素的索引
+    var indexArr = [];
+    $.each($item, function (i, v) {
+      indexArr.push($(v).index() - 1);
     });
-    player.removeMusic(indexs, function (playingIndex) {
-      if(indexs.indexOf(playingIndex) > 0){
-        changeMusic(indexs[indexs.length - 1] + 1);
-      }
-    });
-
-    // 4.1样式上移除
-    $items.remove();
-    //    更新索引
+    // 4.2样式上移除
+    $item.remove();
+    // 4.3更新剩余元素的索引
     $.each($(".menu-item"), function (i, v) {
       $(v).find(".song-index span").text(i + 1);
     });
-    // 4.2逻辑上移除
-    // player.removeMusic(musicIndex, function (playingIndex) {
-    //   if(musicIndex === playingIndex){
-    //     changeMusic(musicIndex);
-    //   }
-    // });
+    // 4.4逻辑上移除
+    player.removeMusic(indexArr, function (playingIndex) {
+      if(indexArr.indexOf(playingIndex) > -1){
+        changeMusic(player.playingIndex + 1);
+      }
+    });
   }
 
   // 4 定义创建音乐条目的方法
@@ -265,5 +263,20 @@ $(function () {
       "          </div>\n" +
       "        </li>");
     return $item;
+  }
+
+  // 获取当前播放模式的方法
+
+  // 获取相应播放模式的下一首歌曲索引的方法
+  function indexWithMode() {
+    var reg = /playing-mode-([a-z]*)/;
+    var $playMode = $(".playing-operate-mode");
+    var curMode = reg.exec($playMode.attr("class"));
+    var nextIndex = {
+      circulate: player.playingIndex + 1,
+      random: Math.floor(Math.random() * player.musicList.length),
+      one: player.playingIndex,
+    };
+    return nextIndex[curMode[1]];
   }
 });
--- Music-Player/js/player.js
+++ Music-Player/js/player.js
@@ -6,9 +6,11 @@
     constructor: Player, // ?
     audio: null, //播放器
     musicList: [], //播放列表
+    modeList: ["circulate", "random", "one"], //播放模式列表
     playing: false, //播放状态
-    playingIndex: null, //正在播放曲目
     playingVolume: null, //播放的音量
+    playingIndex: null, //当前播放曲目
+    playingMode: this.modeList[0], //当前播放模式
     init: function ($audio) {
       this.$audio = $audio;
       this.audio = $audio.get(0);
@@ -26,13 +28,15 @@
     setPlayingVolume: function (volume){
       this.playingVolume = volume;
     },
+    setPlayingMode: function(mode){
+      this.playingMode = mode;
+    },
 
     // 根据索引播放音乐
-    playMusic: function (musicIndex) {
+    playMusic: function (musicIndex, bol) {
       //处理上一首/下一首传来的musicIndex
       musicIndex = (musicIndex + this.musicList.length ) % this.musicList.length;
-
-      if(this.playingIndex === musicIndex){
+      if(this.playingIndex === musicIndex && !bol){
         if(this.audio.paused){
           this.audio.play();
         }else{
@@ -49,17 +53,16 @@
     // 根据索引移除音乐
     removeMusic: function (musicIndex, callback) {
       //包装成数组处理
-      musicIndex = musicIndex instanceof Array ? musicIndex : [musicIndex];
+      var indexArr = musicIndex instanceof Array ? musicIndex : [musicIndex];
       var playingIndex = this.playingIndex;
-      if(musicIndex.indexOf(playingIndex) > 0){
-        var d = musicIndex.length - musicIndex.indexOf(playingIndex);
-        this.setPlayingIndex(playingIndex - d);
+      if(indexArr.indexOf(playingIndex) > -1){
+        this.setPlayingIndex(indexArr[0] - 1);
       }else{
-        if(musicIndex.length - 1  <= playingIndex){
-          this.setPlayingIndex(playingIndex - musicIndex.length);
+        if(indexArr.length - 1  < playingIndex){
+          this.setPlayingIndex(playingIndex - indexArr.length);
         }
       }
-      this.musicList.splice(musicIndex, musicIndex.length);
+      this.musicList.splice(indexArr[0], indexArr.length);
       //将删除歌曲时正在播放的歌曲索引传过去
       callback && callback(playingIndex);
     },
@@ -91,6 +94,13 @@
         this.audio.volume = this.playingVolume;
       }
     },
+
+    // 切换播放模式
+    changeMode: function () {
+      var index = this.modeList.indexOf(this.playingMode);
+      var next = (index + 1) % this.modeList.length;
+      this.setPlayingMode(this.modeList[next]);
+    }
   };
 
   Player.prototype.init.prototype = Player.prototype;
