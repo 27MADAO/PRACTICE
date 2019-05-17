5aef4c18330ad0aa0005717c9070d6ed509a8a8d
MADAO <madao27@163.com>
2019-05-17 23:01:43 +0800

    reorder

--- Music-Player/js/functions.js
+++ Music-Player/js/functions.js
@@ -10,27 +10,6 @@ function parseTime(d) {
   return two(minute) + ":" + two(second);
 }
 
-// 歌词解析 => timeArr/lyricArr
-function parseLyric(data) {
-  var array = data.split("\n");
-  var timeReg = /(\d*:\d*\.\d*)/;
-  var res = {time:[], lyric: []};
-  $.each(array, function (i, v) {
-    var arr = v.split(/\[|]/);
-    var time = arr[1];
-    var lyric = arr[2];
-    // 剔除没有时间或歌词的行数据
-    if(!time.match(timeReg) || !lyric.trim()) return;
-    var timeArr = time.split(":");
-    var min = +timeArr[0];
-    var sec = +timeArr[1];
-    var lyricTime = (min * 60 + sec).toFixed(2);
-    res.time.push(lyricTime);
-    res.lyric.push(lyric);
-  });
-  return res;
-}
-
 // console.log(isNaN(NaN));
 // 0 false
 // "0" false
--- Music-Player/js/lyric.js
+++ Music-Player/js/lyric.js
@@ -15,7 +15,7 @@
         url: url,
         dataType: "text",
         success: function (data) {
-          var res = parseLyric(data);
+          var res = _this.parseLyric(data);
           _this.timeArray = res.time;
           _this.lyricArray = res.lyric;
           callback && callback(_this.lyricArray);
@@ -34,6 +34,27 @@
         }
       });
       return index;
+    },
+
+    // 歌词解析 => timeArr/lyricArr
+    parseLyric: function (data) {
+      var array = data.split("\n");
+      var timeReg = /(\d*:\d*\.\d*)/;
+      var res = {time:[], lyric: []};
+      $.each(array, function (i, v) {
+        var arr = v.split(/\[|]/);
+        var time = arr[1];
+        var lyric = arr[2];
+        // 剔除没有时间或歌词的行数据
+        if(!time.match(timeReg) || !lyric.trim()) return;
+        var timeArr = time.split(":");
+        var min = +timeArr[0];
+        var sec = +timeArr[1];
+        var lyricTime = (min * 60 + sec).toFixed(2);
+        res.time.push(lyricTime);
+        res.lyric.push(lyric);
+      });
+      return res;
     }
   };
   Lyric.prototype.init.prototype = Lyric.prototype;
